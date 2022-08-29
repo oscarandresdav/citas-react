fe7c7f4631cad8ed4645cf4089ffd97a3506feb6
Oscar Davila <oscar7822002@gmail.com>
2022-08-29 16:28:05 -0500

    Creando y llamando nuestro primer Componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import Formulario from "./components/Formulario"
+import Header from "./components/Header"
+
 function App() {
   
   return (
-    <div className="App">
-      <h1>Hola mundo</h1>
-    </div>
+    <>
+      <Header />
+      <Formulario />
+    </>
   )
 }
 
