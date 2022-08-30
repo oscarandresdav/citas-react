ae383cd775a631f90095efb549b4fe080c03e8cc
Oscar Davila <oscar7822002@gmail.com>
2022-08-30 16:09:38 -0500

    Registrando un evento en el Botón de Editar

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,6 +1,6 @@
 import Paciente from "./Paciente"
 
-const ListadoPacientes = ({pacientes}) => {
+const ListadoPacientes = ({pacientes, setPaciente}) => {
 
   return (
     <div className="md:w-1/2 lg:w-3/5">
@@ -19,6 +19,7 @@ const ListadoPacientes = ({pacientes}) => {
               <Paciente 
                 key={paciente.id}
                 paciente={paciente}
+                setPaciente={setPaciente}
               />        
             ))}
           </div>
--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Paciente = ({paciente}) => {
+const Paciente = ({paciente, setPaciente}) => {
 
   const {mascota, propietario, email, fechaAlta, sintomas} = paciente
 
@@ -29,7 +29,8 @@ const Paciente = ({paciente}) => {
 
       <div className='flex justify-between mt-10'>
         <button type='button'
-                className='py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg'>
+                className='py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg'
+                onClick={() => setPaciente(paciente)}>
           Editar
         </button>
         <button type='button'
