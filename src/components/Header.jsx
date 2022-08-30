956199317feaf376d1bdfbe31421b58afa064efe
Oscar Davila <oscar7822002@gmail.com>
2022-08-30 08:47:26 -0500

    Aplicando Classes al header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <header>
-      <h1>Desde Header</h1>
-    </header>
+      <h1 className='font-black text-5xl text-center md:w-2/3 mx-auto'>
+      Seguimiento Pacientes {''}
+      <span className='text-indigo-600'>Veterinaria</span></h1>
   )
 }
 
